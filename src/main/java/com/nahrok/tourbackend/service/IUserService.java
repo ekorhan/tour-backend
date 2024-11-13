b4be78c35a502906da730c3f043e5ed2ae4ca3ff
Mehmet Enes Körhan <mekrh4n@gmail.com>
2024-11-13 04:03:40 +0300

    MOVED TO POSTGRESQL

--- src/main/java/com/nahrok/tourbackend/service/IUserService.java
+++
@@ -1,8 +0,0 @@
-package com.nahrok.tourbackend.service;
-
-import com.nahrok.tourbackend.model.LoginRequest;
-import com.nahrok.tourbackend.model.LoginResponse;
-
-public interface IUserService {
-    LoginResponse login(LoginRequest request) throws InterruptedException;
-}
